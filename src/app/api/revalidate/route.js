bdedd56c32fe6364bde737c2d2497bc8c7581f36
paulovdev <fudevil123@gmail.com>
2025-11-23 19:25:56 -0300

    new revalidate home

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -5,7 +5,6 @@ export async function POST(req) {
   try {
     const body = await req.json();
 
-  
     if (body?.secret !== process.env.SANITY_WEBHOOK_SECRET) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
@@ -13,9 +12,14 @@ export async function POST(req) {
     const slug = body.slug;
     const type = body._type;
 
+    // Revalida works
     revalidateTag("works");
 
+    // Revalida work individual
     if (slug) revalidateTag(`work-${slug}`);
+
+    // Revalida HOME também
+    revalidateTag("home");
 
     return NextResponse.json({
       revalidated: true,
