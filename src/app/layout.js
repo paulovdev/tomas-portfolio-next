95f61841f05b3c82553607c5da1bc74af45d9195
paulovdev <fudevil123@gmail.com>
2025-11-28 17:14:10 -0300

    fivecon

--- src/app/layout.js
+++ src/app/layout.js
@@ -50,6 +50,10 @@ export const metadata = {
 
   icons: {
     icon: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+      },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
