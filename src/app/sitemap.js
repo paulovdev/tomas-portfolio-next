7c57726c325f3d2c686b23a5293f127278e5aa11
paulovdev <fudevil123@gmail.com>
2025-11-27 21:28:27 -0300

    seo 1

---
+++ src/app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://tomasml.com/",
+      lastModified: "2025-11-15",
+      changeFrequency: "yearly",
+      priority: 1.0,
+    },
+    {
+      url: "https://tomasml.com/about",
+      lastModified: "2025-11-15",
+      changeFrequency: "yearly",
+      priority: 0.9,
+    },
+    {
+      url: "https://tomasml.com/works",
+      lastModified: "2025-11-15",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://tomasml.com/contact",
+      lastModified: "2025-11-15",
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+  ];
+}
